c49320998a4e577c221a65aabe1addaa4518c908
Anastasiia Yefremova <a.yefremova@1plus1.tv>
2024-08-01 17:07:43 +0300

    create project

---
+++ BookSummarizer/BookSummarizer/BookSummarizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookSummarizerApp.swift
+//  BookSummarizer
+//
+//  Created by Yefremova on 01.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct BookSummarizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
