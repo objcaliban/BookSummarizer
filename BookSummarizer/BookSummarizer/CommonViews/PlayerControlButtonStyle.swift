86c6773f3c9b935220b5afd9a78b06e8585ea52d
Anastasiia Yefremova <yefremova.a.s@gmail.com>
2024-08-05 22:50:31 +0300

    move constants

--- BookSummarizer/BookSummarizer/CommonViews/PlayerControlButtonStyle.swift
+++ BookSummarizer/BookSummarizer/CommonViews/PlayerControlButtonStyle.swift
@@ -14,12 +14,29 @@ struct PlayerControlButtonStyle: ButtonStyle {
         configuration.label
             .frame(width: height, height: height)
             .animation(nil, value: UUID())
-            .scaleEffect(configuration.isPressed ? 0.9 : 1)
+            .scaleEffect(configuration.isPressed ? Const.Button.animationScale : Const.Button.normalScale)
             .background {
                 Circle()
                     .fill(.gray)
-                    .opacity(configuration.isPressed ? 0.2 : 0)
-                    .scaleEffect(configuration.isPressed ? 1.6 : 1.8)
+                    .opacity(configuration.isPressed ? Const.Background.animationOpacity : Const.Background.normalOpacity)
+                    .scaleEffect(configuration.isPressed ? Const.Background.animationScale : Const.Background.normalScale)
             }
+    }
+}
+
+extension PlayerControlButtonStyle {
+    enum Const {
+        enum Button {
+            static let animationScale: CGFloat = 0.9
+            static let normalScale: CGFloat = 1
+        }
+        
+        enum Background {
+            static let animationScale: CGFloat = 1.6
+            static let normalScale: CGFloat = 1.8
+            
+            static let animationOpacity: CGFloat = 0.2
+            static let normalOpacity: CGFloat = 0
+        }
     }
 }
